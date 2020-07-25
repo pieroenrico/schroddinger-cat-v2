60ee0aef47d8ac4c8c4ce5d8b40bb9885820d935
piero <piero.bona@gmail.com>
2020-07-25 11:43:02 -0500

    initial

---
+++ Assets/Scripts/Utils/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Helper : MonoBehaviour
+{
+
+    public string label;
+    public bool hideOnStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hideOnStart)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (label != null && label != "")
+        {
+            Handles.Label(transform.position, label);
+        }
+    }
+}
